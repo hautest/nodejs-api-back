0a0c3b5df66537c3b8ed54940677be28071ebddf
LeeSeungHun <dos07008@naver.com>
2023-02-15 00:51:26 +0900

    mysql node js 연동, todolist 만들기

--- api.js
+++ api.js
@@ -1,27 +1,66 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
+require("dotenv").config();
 
-app.listen(8080, function () {
-  console.log("server listening on port 3000");
-});
+var mysql = require("mysql");
 
 app.use(express.json());
 app.use(cors());
 
-const uri =
-  "mongodb+srv://lee:parangse29@todolist.myluw50.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
+var connection = mysql.createConnection({
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: "opentutorials",
 });
-client
-  .connect((err) => {
-    const collection = client.db("test").collection("devices");
-    client.close();
-  })
-  .then(() => {
-    console.log("mongoDB 연결 !");
+
+app.listen(process.env.PORT, function () {
+  console.log("Server 실행");
+});
+
+app.get("/list", (req, res) => {
+  connection.query("SELECT * from TodoList", (error, rows) => {
+    if (error) throw error;
+    res.send(rows);
   });
+});
+
+app.post("/create-list", (req, res) => {
+  const value = req.body.value;
+  connection.query(
+    `INSERT INTO TodoList (checked, value) VALUES (false,'${value}') `,
+    (error, rows) => {
+      if (error) throw error;
+      connection.query("SELECT * from TodoList", (error, rows) => {
+        if (error) throw error;
+        res.send(rows);
+      });
+    }
+  );
+});
+
+app.patch("/update-list", (req, res) => {
+  const id = req.body.id;
+  connection.query(
+    `UPDATE TodoList SET checked = !checked WHERE id = ${id}`,
+    (error, rows) => {
+      if (error) throw error;
+      connection.query("SELECT * from TodoList", (error, rows) => {
+        if (error) throw error;
+        res.send(rows);
+      });
+    }
+  );
+});
+
+app.delete("/list/:id", (req, res) => {
+  const id = req.params.id;
+  connection.query(`DELETE FROM TodoList WHERE id = ${id}`, (error, row) => {
+    if (error) throw error;
+    connection.query("SELECT * from TodoList", (error, rows) => {
+      if (error) throw error;
+      res.send(rows);
+    });
+  });
+});
